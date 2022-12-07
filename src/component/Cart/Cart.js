8a47b8e88b540164c353cbe05c958a30168b0959
Muhammad Tamim <mdt40234@gmail.com>
2022-12-07 13:37:16 +0600

    total price , shipping ,tax and Grand total

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import "./Cart.css"
 
-const Cart = (Cart) => {
+const Cart = (Carts) => {
+    console.log(Carts.Cart);
+    let totalPrice = 0;
+    let shipping = 0;
+    let tex = 0;
+    let GrandTotal = 0;
+    for (let cart of Carts.Cart) {
+        totalPrice += cart.price;
+        shipping += cart.shipping;
+        tex = totalPrice / 10;
+        GrandTotal = totalPrice + shipping + tex;
+    }
     return (
         <div className='Cart'>
             <div>
@@ -9,7 +20,12 @@ const Cart = (Cart) => {
                 <h2 className='order-title'>Order Summary</h2>
                 <br />
                 <div className="order-info">
-                    <p>Selected Items: {Cart.Cart.length}</p>
+                    <p>Selected Items: {Carts.Cart.length}</p>
+                    <p>Total Price: ${totalPrice}</p>
+                    <p>Shiping charge: ${shipping}</p>
+                    <p>Tex: ${tex}</p>
+                    <br />
+                    <h3>Grand Total: ${GrandTotal}</h3>
                 </div>
             </div>
         </div>
--- src/component/shop/Shop.js
+++ src/component/shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Product from '../../products/Products';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import './Shop.css'
 
@@ -16,6 +17,7 @@ const Shop = () => {
         console.log(product);
         const newCart = [...cart, product]
         setCart(newCart)
+        addToDb(product.id)
     }
     return (
         <div className='shop-containar'>
