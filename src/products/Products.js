d926a94a142030111b701066b1cdd06ac6e62845
Muhammad Tamim <mdt40234@gmail.com>
2022-12-09 11:40:31 +0600

    added rating

--- src/products/Products.js
+++ src/products/Products.js
@@ -1,6 +1,7 @@
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import Rating from 'react-rating';
 import './Products.css'
 
 const Product = (props) => {
@@ -16,7 +17,12 @@ const Product = (props) => {
                     <h4>Price :${props.product.price}</h4>
                     <div className="sub-info">
                         <p><small>Manufacturer : {props.product.seller}</small></p>
-                        <p><small>Rating : {props.product.ratings}</small></p>
+                        <p><small>Rating :<Rating
+                            initialRating={props.product.ratings}
+                            readonly
+                            emptySymbol='fa-solid fa-star-of-life'
+                            fullSymbol='fa-solid fa-star'
+                        ></Rating> </small></p>
                     </div>
                 </div>
                 <div className="shop-button">
